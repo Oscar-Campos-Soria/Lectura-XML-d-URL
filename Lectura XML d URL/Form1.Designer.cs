bfba40402e5f2fea0cf78199463aa55b11fdfbb7
FALKON <campossoriao1@gmail.com>
2024-05-02 02:30:21 +0200

    Agregar archivos de proyecto.

---
+++ Lectura XML d URL/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Lectura_XML_d_URL
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
+            this.listViewXMLData = new System.Windows.Forms.ListView();
+            this.Title = new System.Windows.Forms.ColumnHeader();
+            this.info = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // listViewXMLData
+            // 
+            this.listViewXMLData.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listViewXMLData.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Title,
+            this.info});
+            listViewGroup1.Header = "ListViewGroup";
+            listViewGroup1.Name = "listViewGroup1";
+            this.listViewXMLData.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1});
+            this.listViewXMLData.HideSelection = false;
+            this.listViewXMLData.Location = new System.Drawing.Point(15, 15);
+            this.listViewXMLData.Margin = new System.Windows.Forms.Padding(6);
+            this.listViewXMLData.Name = "listViewXMLData";
+            this.listViewXMLData.Size = new System.Drawing.Size(676, 354);
+            this.listViewXMLData.TabIndex = 1;
+            this.listViewXMLData.UseCompatibleStateImageBehavior = false;
+            this.listViewXMLData.View = System.Windows.Forms.View.Details;
+            // 
+            // Title
+            // 
+            this.Title.Tag = "Title";
+            this.Title.Text = "Title";
+            this.Title.Width = 100;
+            // 
+            // info
+            // 
+            this.info.Tag = "info";
+            this.info.Text = "Info";
+            this.info.Width = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 451);
+            this.Controls.Add(this.listViewXMLData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView listViewXMLData;
+        private System.Windows.Forms.ColumnHeader Title;
+        private System.Windows.Forms.ColumnHeader info;
+    }
+}
+
---
+++ Lectura XML d URL/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+
+namespace Lectura_XML_d_URL
+{
+    public partial class Form1 : Form
+    {
+        String URLString;
+        XmlTextReader reader;
+        public Form1()
+        {
+            InitializeComponent();
+            URLString = "https://www.w3schools.com/xml/cd_catalog.xml";
+            reader = new XmlTextReader(URLString);
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: 
+                        ListViewItem item = new ListViewItem(reader.Name);
+
+                        if (reader.HasAttributes)
+                        {
+                            while (reader.MoveToNextAttribute())
+                            {
+                                item.SubItems.Add(reader.Name + ": " + reader.Value);
+                            }
+
+                           
+                            reader.MoveToElement();
+                        }
+
+                        listViewXMLData.Items.Add(item);
+                        break;
+
+                    case XmlNodeType.Text: 
+                        listViewXMLData.Items[listViewXMLData.Items.Count - 1].SubItems.Add( ""+ reader.Value);
+                        break;
+
+                    case XmlNodeType.EndElement: 
+                        break;
+                }
+            }
+        }
+
+        private void listViewXMLData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
